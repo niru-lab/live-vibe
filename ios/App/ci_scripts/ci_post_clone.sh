aac4a443027309a870551f68a6255d3eaf89d71d
Niruban Yeyavathanan <niru@air-von-niruban.speedport.ip>
2026-08-23 14:15:13 +0200

    fix: disable CI scripts, node_modules now in repo

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -1,12 +1,2 @@
 #!/bin/sh
-set -e
-
-echo "Finding Node..."
-which node || true
-ls /usr/local/bin/node || true
-ls /usr/bin/node || true
-ls ~/.nvm/versions/ || true
-
-echo "PATH: $PATH"
-
-echo "Done."
+exit 0
--- ios/App/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/App/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-set -e
-cd "$CI_WORKSPACE"
-npm install
-npx cap sync ios
+exit 0
